e6a5da4db73c530c7e49d55ab367716b4ea2b685
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2026-01-29 12:16:16 +0000

    Deploying to gh-pages from @ fabianvanrissenbeck/memclave@0eb3f064a6ebcefa147e6bf9a6abb25439e80ae0 🚀

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['generating_20the_20plot_0',['Generating The Plot',['../memclave-reprod.html#autotoc_md13',1,'']]],
-  ['guide_20for_20memclave_1',['Reproduction Guide for Memclave',['../memclave-reprod.html',1,'']]]
+  ['guide_1',['New Guide',['../memclave-reprod.html#autotoc_md6',1,'']]],
+  ['guide_20for_20memclave_2',['Reproduction Guide for Memclave',['../memclave-reprod.html',1,'']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['n_0',['n',['../vud__ime_8h.html#a3d05cf6aa68b03e254d70afbe869e305',1,'vud_ime.h']]],
-  ['next_5fsk_1',['next_sk',['../structvud__rank.html#a3f14156180691cca022213ba6e6ce285',1,'vud_rank']]]
+  ['new_20guide_1',['New Guide',['../memclave-reprod.html#autotoc_md6',1,'']]],
+  ['next_5fsk_2',['next_sk',['../structvud__rank.html#a3f14156180691cca022213ba6e6ce285',1,'vud_rank']]]
 ];
