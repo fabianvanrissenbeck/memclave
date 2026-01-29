8b063734e5df34dd1c51be822c1fb54bd2a5258d
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2026-01-29 21:51:14 +0100

    fixed incorrect qcow2 path/--exact-rank for boot.sh

--- scripts/hyp/boot.sh
+++ scripts/hyp/boot.sh
@@ -5,8 +5,14 @@ HYP_PATH=$(realpath $(dirname "$0"))
 QEMU_PATH="$HYP_PATH/bin/qemu-system-x86_64"
 LIBRARY_PATH="${HYP_PATH}/bin"
 
-UPMEM_VERBOSE="" ${HYP_PATH}/bin/ci-switch &
-CI_SWITCH_PID=$!
+if [ "$#" == "1" ];
+then
+    UPMEM_VERBOSE="" ${HYP_PATH}/bin/ci-switch "$1" &
+    CI_SWITCH_PID=$!
+else
+    UPMEM_VERBOSE="" ${HYP_PATH}/bin/ci-switch &
+    CI_SWITCH_PID=$!
+fi
 
 cd bin
 sleep 1
@@ -14,7 +20,7 @@ sleep 1
 LD_LIBRARY_PATH=$LIBRARY_PATH $QEMU_PATH \
     --nographic \
     --enable-kvm \
-    -drive file="${HYP_PATH}/debian.qcow2",if=virtio,media=disk \
+    -drive file="${HYP_PATH}/memclave.qcow2",if=virtio,media=disk \
     -cpu host \
     -smp $(nproc) \
     -m 16G
